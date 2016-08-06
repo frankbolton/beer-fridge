dd16ba13794432b3ee3dba65b6e5fe0b2747703f
frank bolton <frankjbolton@gmail.com>
2016-08-06 21:06:31 +0300

    Updated to work with SainSmart LCDKeypadShield and with the no pullup resistor 1-wire driver

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ It has a stupid simple hysteresis controller (bang bang).
 The arduino and solid state relay are mounted inside of a multiplug with the LCD visible.
 At some stage the LCD Failed, so I used a Nexus 7 tablet and USB OTG to check on the temperature.
 
+Older commits deal with this
+
 Photos:
 
 http://imgur.com/9svZOD1
@@ -18,3 +20,9 @@ http://imgur.com/cocHXIF
 http://imgur.com/5LsrCcs
 
 http://imgur.com/gAmo9uQ
+
+
+The second revision takes place in 2016. I'm now a married man who bought a fridge specifically 
+for fermentation. This versuion once again relies on an Arduino Uno, a DS120 temperature sensor, 
+a Sainsmart LCD Keypad shield and an El-cheapo SSR.
+
--- fridge_thermo/fridge_thermo.ino
+++ fridge_thermo/fridge_thermo.ino
@@ -1,151 +1,110 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-// Data wire is plugged into pin 10 on the Arduino
-#define ONE_WIRE_BUS 10
+#include <DFR_Key.h>
+#include <LiquidCrystal.h>
 
+// Data wire is plugged into port x on the Arduino
+#define ONE_WIRE_BUS 3
+//#define DEBUG true
+#define FRIDGE_CTRL_PIN 12
 
-// Setup a oneWire instance to communicate with any OneWire devices
+// Uncomment this line if you are using the updated dallas_temp_library that 
+// supports the busFail() method to diagnose bus problems
+// #define BUSFAIL 
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
-boolean o1;
-
-
 
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-/*
-ROM = 28 E3 34 7F 4 0 0 59
-  Chip = DS18B20
-  Data = 1 C9 1 4B 46 7F FF 7 10 64  CRC=64
-  Temperature = 28.56 Celsius, 83.41 Fahrenheit
-ROM = 28 F A1 EB 3 0 0 FD
-  Chip = DS18B20
-  Data = 1 C8 1 4B 46 7F FF 8 10 3F  CRC=3F
-  Temperature = 28.50 Celsius, 83.30 Fahrenheit
-No more addresses.
-*/
 
-  DeviceAddress firstThermometer = {  
-    0x28, 0xE3, 0x34, 0x7F, 0x04, 0x00, 0x00, 0x59 };
-  
-  DeviceAddress secondThermometer = {
-    0x28, 0x0F, 0xA1, 0xEB, 0x03, 0x00, 0x00, 0xFD };
-  int fridgeControlPin = 8;
-  
-  
-  long previousMillis = 0;
-  long interval = 20;           //number of seconds between readings
-  long previousMillisStore = 0;
-  long intervalStore = 90;      //store every 15 minutes : 15*60 = 900
-  float FridgeHighSP = 2.5E1;  //this is the temperature at which we turn on the fridge
-  float FridgeLowSP = 1.9E1;   //this is the temperature at which we turn off the fridge
-  float temp1;
-  const int NumberQuantity = 10;
-  float tempLog[NumberQuantity];
-  
-  
-  void setup()
+DFR_Key keypad;
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+int localKey = 0;
+int sp_h =20;
+int sp_l = 0;
+
+bool cooling = false;
+
+// initialize the library with the numbers of the interface pins
+
+
+void setup()
 {
+
+  // This lines just make it so you can plug a DS18B20 directly into 
+  // digitial pins 8-10. 
+  
+  digitalWrite( ONE_WIRE_BUS-1 , LOW );
+  pinMode( ONE_WIRE_BUS-1  , OUTPUT );
+  digitalWrite( ONE_WIRE_BUS+1 , LOW );  
+  pinMode( ONE_WIRE_BUS+1 , OUTPUT );
+
+  //Setup Fridge output pin and neighbor as ground to allow direct connection
+  pinMode( FRIDGE_CTRL_PIN , OUTPUT);
+  digitalWrite (FRIDGE_CTRL_PIN, LOW);
+  pinMode( FRIDGE_CTRL_PIN+1, OUTPUT);
+  digitalWrite(FRIDGE_CTRL_PIN+1, LOW);
+   // start serial port
   Serial.begin(9600);
-  //sensors.begin();
-  // set the resolution to 10 bit (good enough?)
-  sensors.setResolution(firstThermometer, 10);
-  sensors.setResolution(secondThermometer, 10);
-  
-  Serial.println("thermometer initialized");
+  Serial.println("Dallas Temperature IC Control Library Demo");
 
-    // initialize the digital pin as an output.
-  pinMode(fridgeControlPin, OUTPUT);  
-  
-  //tempLog[1] = 5.6;
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Beer controller");
+  keypad.setRate(10);
+  delay(1000);
+
 }
 
+void loop(){
+  //load the temperature from the DS18B20 (only one on the network at this stage)
+  DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address    
+  // For testing purposes, reset the bus every loop so we can see if any devices appear or fall off
+  sensors.begin();
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  sensors.getAddress(tempDeviceAddress, 1);
+  float tempC = sensors.getTempC(tempDeviceAddress);
+  //Serial.println(tempC); // Converts tempC to Fahrenheit
+  int tempI = int(tempC);
+  Serial.println(tempI);
 
-void loop()
-{
-//String dataString;
-  unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval*1000) {
-    // save the last time you updated saved and sent data
-    previousMillis = currentMillis;   
+  //Handle changes to the setpoint
+  localKey = keypad.getKey();
+  if (localKey != SAMPLE_WAIT) {
+    //put the code here to change the setpoint
+    if (localKey == 3) sp_h ++;
+    if (localKey == 4) sp_h --;
+    sp_l = sp_h - 2;
+    delay(300);
+  }  
 
-    Serial.print("Getting temperatures...\n\r");
-    sensors.requestTemperatures();
 
-    // make a string for assembling the data to log:
-  ///  dataString = String(previousMillis/1000) + ",";
 
-    //FIRST THERMOMETER
-    int tempC_dec = 100*sensors.getTempC(firstThermometer);
-    int tempC_int = sensors.getTempC(firstThermometer);
-    String tempString = String(tempC_int)+"."+String(tempC_dec-100*tempC_int);
-    Serial.println("Temperature 1= "+tempString);
-    temp1 = sensors.getTempC(firstThermometer);
-    //Serial.println("T1 float = "+temp1);
-    
-    
-    
-    //SECOND THERMOMETER
-    tempC_dec = 100*sensors.getTempC(secondThermometer);
-    tempC_int = sensors.getTempC(secondThermometer);
-    tempString = String(tempC_int)+"."+String(tempC_dec-100*tempC_int);
-    Serial.println("Temperature 2= "+tempString);
-    //float temp2 = sensors.getTempC(secondThermometer); 
- 
- 
-     //control logic. 
-    //---------------------------------------------
-    //
-    //  TEMP1 is the sensor that goes inside the fridge
-    //
-    //
-    //--------------------------------------------
-    if (temp1>FridgeHighSP){
-      digitalWrite(fridgeControlPin, HIGH);
-      o1 =1;
-    } else if (temp1<FridgeLowSP){
-      digitalWrite(fridgeControlPin, LOW);
-      o1=0;
+  //Control the fridge
+  if (tempI >= sp_h){
+      digitalWrite(FRIDGE_CTRL_PIN, HIGH);
+      cooling = true;
+     } else if (tempI<sp_l){
+      digitalWrite(FRIDGE_CTRL_PIN, LOW);
+      cooling = false;
     }
-    
-    if (o1){
-      Serial.println("The fridge is ON");
-    }else{
-      Serial.println("The fridge is OFF");
-    }
-    
-    //for(int i = 5; i == 0; i--){
-    //    tempLog[i] = tempLog[i-1];
-    // }
-    //tempLog[0] = temp1;
-  
-  }
-    
-      if(currentMillis - previousMillisStore > intervalStore*1000) {
-    // save the last time you updated saved and sent data
-    previousMillisStore = currentMillis;   
-     //tempLog[10]=tempLog[9];
-     tempLog[9]=tempLog[8];
-     tempLog[8]=tempLog[7];
-     tempLog[7]=tempLog[6];
-     tempLog[6]=tempLog[5];
-     tempLog[5]=tempLog[4];
-     tempLog[4]=tempLog[3];
-     tempLog[3]=tempLog[2];
-     tempLog[2]=tempLog[1];
-     tempLog[1]=tempLog[0];
-     tempLog[0]=temp1;
-     //Serial.println("The last 10 (15 minute blocks) temperatures are: ");
-    //for(int i = 0; i < NumberQuantity-1; i++){
-    //  Serial.print(tempLog[i]);
-    //  Serial.print(",");
-    //}
-    // Serial.println();
-    }  
-    //Serial.write("The fridge is turned :",  
-    
-    
-  
+
+      //Print the display contents
+  //clear the LCD
+  lcd.clear();
+  lcd.print("Beer Controller");
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print("Temp:");
+  //lcd.setCursor(6, 1);
+  lcd.print(tempI);
+  lcd.setCursor(9, 1);
+  lcd.print("SP:");
+  lcd.print(sp_h);
+  lcd.setCursor(15, 1);
+  if (cooling == HIGH) lcd.print("+");
+  else lcd.print("_");
 }
-
-
 
